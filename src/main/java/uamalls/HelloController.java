1626636311a9c9185212df388af496c58464d382
Dmytro Shevchenko <Dmytro_Shevchenko1@epam.com>
2018-01-10 12:56:13 +0200

    update spring boot to 2.0.0.M7

--- src/main/java/uamalls/HelloController.java
+++ src/main/java/uamalls/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String index() {
+    public String hello() {
         return "Greetings from Spring Boot!";
     }
 
