477495fc54ef9f1aebaa4ba26ab26d7d746cdfc0
Dmytro Shevchenko <Dmytro_Shevchenko1@epam.com>
2018-01-11 12:58:14 +0200

    add thymeleaf; add devtools

--- src/main/java/uamalls/HelloController.java
+++ src/main/java/uamalls/HelloController.java
@@ -1,14 +1,23 @@
 package uamalls;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HelloController {
 
     @RequestMapping("/hello")
+    @ResponseBody
     public String hello() {
         return "Greetings from Spring Boot!";
     }
 
+    @RequestMapping("/welcome")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", "SHEVCHENKO");
+        return "welcome";
+    }
 }
